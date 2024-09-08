803ec2d78e3f38472b310350fe65f61f8b83d712
Sonic853 <Sonic853@qq.com>
2024-09-08 22:23:18 +0800

    edit demo

--- Scripts/Test/Test.cs
+++ Scripts/Test/Test.cs
@@ -2,14 +2,17 @@
 using Sonic853.Texture2String;
 using UdonSharp;
 using UnityEngine;
+using UnityEngine.UI;
 using VRC.SDKBase;
 using VRC.Udon;
 
-namespace Sonic853
+namespace Sonic853.Texture2String.Test
 {
     public class Test : UdonSharpBehaviour
     {
         public Texture2D texture;
+        public Text text;
+        public UdonSharpBehaviour imageDisplayer;
         public bool useLength;
         public bool evenStorage;
         public int mode;
@@ -36,15 +39,37 @@ namespace Sonic853
         }
         public void TestToText()
         {
-            Debug.Log(texture.ToText(useLength, evenStorage));
+            var t = texture.ToText(useLength, evenStorage);
+            Debug.Log(t);
+            text.text = t;
+            if (imageDisplayer != null)
+            {
+                imageDisplayer.SetProgramVariable("texture", texture);
+                imageDisplayer.SendCustomEvent("SetTexture");
+            }
         }
         public void TestRGBAToText()
         {
+            var t = texture.RGBAToText(useLength, evenStorage);
+            Debug.Log(t);
+            text.text = t;
             Debug.Log(texture.RGBAToText(useLength, evenStorage));
+            if (imageDisplayer != null)
+            {
+                imageDisplayer.SetProgramVariable("texture", texture);
+                imageDisplayer.SendCustomEvent("SetTexture");
+            }
         }
         public void TestAlphaToText()
         {
-            Debug.Log(texture.AlphaToText(useLength, evenStorage));
+            var t = texture.AlphaToText(useLength, evenStorage);
+            Debug.Log(t);
+            text.text = t;
+            if (imageDisplayer != null)
+            {
+                imageDisplayer.SetProgramVariable("texture", texture);
+                imageDisplayer.SendCustomEvent("SetTexture");
+            }
         }
     }
 }
